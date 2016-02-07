b6d26cf46ec7809c634bfa8b22bc7a1a798e44f3
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-06 22:27:22 -0500

    Adds __ini__.py

---
+++ nwid/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#                        _
+#    _ ____      _(_) __| |
+#   | '_ \ \ /\ / / |/ _` |
+#   | | | \ V  V /| | (_| |
+#   |_| |_|\_/\_/ |_|\__,_|
+#
+
+"""
+nwid
+~~~~
+
+An experimental terminal widget (curses) manger for humans.
+"""
+
+from __future__ import absolute_import
+
+from .app import App
+from . import widget
+
+
+__title__ = 'nwid'
+__version__ = '0.0.1'
+__author__ = 'Harold Bradley III'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Harold Bradley III'
