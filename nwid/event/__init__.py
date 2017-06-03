964327c3589881059055407aac575384818e912b
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-06-02 21:19:24 -0400

    Adds raw_terminal context manager.

--- nwid/event/__init__.py
+++ nwid/event/__init__.py
@@ -11,3 +11,21 @@ This module contains objects and data structures necessary for handling events.
 from __future__ import absolute_import
 
 from .keyboard import *
+
+
+
+# TODO: This should probably be moved to the terminal module
+from contextlib import contextmanager
+
+@contextmanager
+def raw_terminal():
+    input = sys.stdin.fileno()
+    output = sys.stdout.fileno()
+    original_settings = termios.tcgetattr(fd)  # Save original settings to restore them before quitting
+    try:
+        tty.setraw(fd)
+        yield input, output
+    finally:
+        # restore terminal settings. Do this when all output is
+        # finished - TCSADRAIN flag
+        termios.tcsetattr(fd, termios.TCSADRAIN, original_settings)
