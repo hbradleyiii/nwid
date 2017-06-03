74748177dda23ad0f6b4a11042b526eb0447606a
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-06-02 23:03:42 -0400

    Adds HandlerList for event handling.

--- nwid/event/__init__.py
+++ nwid/event/__init__.py
@@ -11,6 +11,7 @@ This module contains objects and data structures necessary for handling events.
 from __future__ import absolute_import
 
 from .event import Event
+from .handler_list import HandlerList
 from .keyboard import *
 
 
