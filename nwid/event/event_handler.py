1368583c3245e57666d14f589fe55c5768ae44d0
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-06-06 22:41:51 -0400

    Adds EventHandler class and tests.

---
+++ nwid/event/event_handler.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# name:             events.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       06/02/2017
+#
+
+"""
+nwid.events
+~~~~~~~~~~~
+
+The event handler is a mixin to be exteneded by an object that should be able
+to handle events.
+"""
+
+
+from __future__ import absolute_import
+
+from .handler_list import EVENT_BUBBLE, EVENT_CAPTURE, HandlerList
+
+
+class EventHandler(object):
+    """This class defines an object that can handle events.
+    It is intended to be extended by a widget like object.
+    """
+
+    # TODO: int events, but how to do super with mulitple??
+
+    def __init__(self, *args, **kwargs):
+        """Initializes an empty collection of events."""
+        self.events = {}
+
+    def register(self, event_name, callback_func, priority=50, identifier=None,
+                method=EVENT_BUBBLE):
+        """Registers an event with a callback function on this widget.
+        An event may have more than one callback function in an ordered list
+        arranged by priority.
+
+        Events are mapped to a HandlerList in the dict self.events
+
+        :param event_name: The name of the event.
+        :param callback_func: A callback function. It must take exactly one
+            argument. (the event object)
+        :param priority: The order of priority for event handlers. Takes an
+            integer (by convention 0-99).  0 will be called first and 99 is
+            called last. This defaults to 50. Callbacks with the same priority
+            will be run in the order in which they were originally registered.
+        :param identifier: An optional string identifier for the event.
+        """
+        if event_name in self.events:
+            self.events[event_name].add(callback_func, priority, identifier,
+                                        method)
+        else:
+            self.events[event_name] = HandlerList(callback_func, priority,
+                                                  identifier, method)
+
+    def unregister(self, event_name, callback_func=None, identifier=None):
+        """Unregisters an event handler.
+
+        If a callback_func is given, only the callback_func attached to the
+        event is removed. Otherwise, all handlers for that particular event are
+        removed.
+
+        :param event_name: The name of the event.
+        :param callback_func: A callback function. It must take exactly one
+            argument. (the event object)
+        :param identifier: The identifier of the handler to unregister.
+        """
+        if callback_func:
+            self.events[event_name].remove(callback_func)
+        else:
+            del self.events[event_name]
+
+    def run_handlers(self, event, method=EVENT_CAPTURE):
+        """Runs all callbacks for events with the passed propagation method."""
+        if event not in self.events:
+            return None
+        for handler in self.events[str(event)].with_method(method):
+            handler(event)
+
+    # TODO, event passed here should be an event object NOT a string.
+
+    def trigger(self, event=None, method=EVENT_CAPTURE):
+        """Called to handle events that have been fired.
+
+        If the (propagation) method flag is set to EVENT_CAPTURE, this function
+        will run any handler for the event that has the method flag set to
+        EVENT_CAPTURE. Then, if the object has children, it will trigger the
+        event with the EVENT_CAPTURE flag on the focused child object.
+
+        If it doesn't have children, it will run any handler for the event that
+        has the method flag set to EVENT_BUBBLE. Then, if the object has a
+        parent, it will trigger the event with the EVENT_BUBBLE flag on the
+        parent.
+
+        If this method is called with the method flag set to EVENT_BUBBLE,
+        the EVENT_CAPTURE method is skipped. After the last parent, the
+
+        Note that the propagation can be stopped by any of the handlers by
+        raising the PreventDefault Exception (although this should be used
+        sparingly).
+
+        :param event: An object representing the event that occurred.
+        """
+
+        # Event Capturing
+        if method == EVENT_CAPTURE:
+            self.run_handlers(event, EVENT_CAPTURE)
+
+            # Let children capture the event
+            if self.has_child():
+                self.focused_child.trigger(event, EVENT_CAPTURE)
+
+            else: # At the last child, start the bubbling
+                self.trigger(event, EVENT_BUBBLE)
+
+        # Event Bubbling
+        else:
+            self.run_handlers(event, EVENT_BUBBLE)
+
+            # Let the event bubble to the parents
+            if self.has_parent():
+                self.parent.trigger(event, EVENT_BUBBLE)
+
+    def has_child(self):
+        """This is a stub that implies the class cannot have children.
+        If the class has children, this should method be implemented by the class."""
+        return False
+
+    def has_parent(self):
+        """This is a stub that implies the class cannot have a parent.
+        If the class has a parent, this should method be implemented by the class."""
+        return False
+
+    @property
+    def focused_child(self):
+        """This property must be implemented by subclasses that can have
+        children."""
+        raise NotImplementedError('Property must be implemented by subclass \
+                                  of EventHandler.')
+
+    @property
+    def parent(self):
+        """This property must be implemented by subclasses that can have
+        children/parents."""
+        raise NotImplementedError('Property must be implemented by subclass \
+                                  of EventHandler.')
--- tests/event/test_event_handler.py
+++ tests/event/test_event_handler.py
@@ -13,16 +13,76 @@ Unittests for nwid.events.event_handler module.
 
 from __future__ import absolute_import
 
-from mock import patch
-from nwid.event import EventHandler
+from nwid.event import EventHandler, EVENT_BUBBLE, EVENT_CAPTURE
 import pytest
 
 
-def mock_callback():
+## Mock Functions/Classes ##
+
+def mock_callback(_=None):
+    Mock.mock_callback_was_called = True
     return True
 
-def mock_alt_callback():
+def mock_alt_callback(_=None):
+    Mock.mock_alt_callback_was_called = True
     return False
+
+class Mock():
+    mock_callback_was_called = False
+    mock_alt_callback_was_called = False
+
+def Mock_reset():
+    Mock.mock_callback_was_called = False
+    Mock.mock_alt_callback_was_called = False
+
+class TestImplementationOfEventHandler(EventHandler):
+    """Mock Implementation of an EventHandler."""
+    def __init__(self, *args, **kwargs):
+        super(TestImplementationOfEventHandler, self).__init__(*args, **kwargs)
+        self._focused_child = None
+        self._parent = None
+
+    @property
+    def focused_child(self):
+        return self._focused_child
+
+    @property
+    def parent(self):
+        return self._parent
+
+    def has_child(self):
+        return True if self._focused_child else False
+
+    def has_parent(self):
+        return True if self._parent else False
+
+class TestCallback(object):
+    def __init__(self):
+        self.order = []
+
+    def c1(self, _):
+        self.order.append(1)
+
+    def c2(self, _):
+        self.order.append(2)
+
+    def c3(self, _):
+        self.order.append(3)
+
+    def c4(self, _):
+        self.order.append(4)
+
+    def c5(self, _):
+        self.order.append(5)
+
+    def c6(self, _):
+        self.order.append(6)
+
+    def c7(self, _):
+        self.order.append(7)
+
+    def c8(self, _):
+        self.order.append(8)
 
 
 ## Test mixin object EventHandler ##
@@ -33,17 +93,180 @@ def test_EventHandler_is_initialized_with_empty_dict():
     event_handler = EventHandler()
     assert event_handler.events == {}
 
-
 def test_EventHandler_can_register_a_new_event():
     """An EventHandler is initialized with an empty collection of events
     mapped to each handler list."""
     event_handler = EventHandler()
     event_handler.register('event', mock_callback)
-    assert event_handler.events['event'].callback_func == mock_callback
+    assert mock_callback in event_handler.events['event']
 
 def test_EventHandler_can_register_multiple_callbacks_to_one_event():
     """An EventHandler can register more than one callback to a particular
     event."""
     event_handler = EventHandler()
     event_handler.register('event', mock_callback)
-    assert event_handler.events['event'] == mock_callback
+    event_handler.register('event', mock_alt_callback)
+    assert mock_callback in event_handler.events['event']
+    assert mock_callback in event_handler.events['event']
+
+def test_EventHandler_can_unregister_an_event():
+    """An EventHandler can unregister an event."""
+    event_handler = EventHandler()
+    event_handler.register('event', mock_callback)
+    event_handler.register('event', mock_alt_callback)
+    event_handler.unregister('event', mock_callback)
+    assert mock_callback not in event_handler.events['event']
+    assert mock_alt_callback in event_handler.events['event']
+    event_handler.unregister('event', mock_alt_callback)
+    assert mock_alt_callback not in event_handler.events['event']
+
+def test_EventHandler_can_run_handlers_for_an_event_bubbling():
+    Mock_reset()
+
+    event_handler = EventHandler()
+    event_handler.register('event', mock_callback)  # Using default method
+    event_handler.register('event', mock_alt_callback, method=EVENT_BUBBLE)
+
+    assert not event_handler.run_handlers('no_handlers', method=EVENT_BUBBLE)
+
+    assert not Mock.mock_callback_was_called
+    assert not Mock.mock_alt_callback_was_called
+
+    event_handler.run_handlers('event', EVENT_BUBBLE)
+
+    assert Mock.mock_callback_was_called
+    assert Mock.mock_alt_callback_was_called
+
+def test_EventHandler_can_run_handlers_for_an_event_capturing():
+    Mock_reset()
+
+    event_handler = EventHandler()
+    event_handler.register('event', mock_callback, method=EVENT_CAPTURE)
+    event_handler.register('event', mock_alt_callback, method=EVENT_CAPTURE)
+
+    assert not Mock.mock_callback_was_called
+    assert not Mock.mock_alt_callback_was_called
+
+    event_handler.run_handlers('event', EVENT_CAPTURE)
+
+    assert Mock.mock_callback_was_called
+    assert Mock.mock_alt_callback_was_called
+
+def test_EventHandler_can_trigger_an_event():
+    Mock_reset()
+
+    event_handler = EventHandler()
+    event_handler.register('event', mock_callback, method=EVENT_CAPTURE)
+    event_handler.register('event', mock_alt_callback, method=EVENT_BUBBLE)
+
+    assert not Mock.mock_callback_was_called
+    assert not Mock.mock_alt_callback_was_called
+
+    event_handler.trigger('event')
+
+    assert Mock.mock_callback_was_called
+    assert Mock.mock_alt_callback_was_called
+
+def test_EventHandler_can_trigger_an_event_on_children():
+    Mock_reset()
+
+    eh_parent = TestImplementationOfEventHandler()
+    eh_child = TestImplementationOfEventHandler()
+    eh_grandchild = TestImplementationOfEventHandler()
+
+    eh_parent._focused_child = eh_child
+    eh_child._focused_child = eh_grandchild
+
+    eh_child._parent = eh_parent
+    eh_grandchild._parent = eh_child
+
+    eh_child.register('event', mock_callback)
+    eh_grandchild.register('event', mock_alt_callback)
+
+    assert not Mock.mock_callback_was_called
+    assert not Mock.mock_alt_callback_was_called
+
+    eh_parent.trigger('event')
+
+    assert Mock.mock_callback_was_called
+    assert Mock.mock_alt_callback_was_called
+
+def test_EventHandler_can_bubble_events_up_to_parents():
+    Mock_reset()
+
+    eh_parent = TestImplementationOfEventHandler()
+    eh_child = TestImplementationOfEventHandler()
+    eh_grandchild = TestImplementationOfEventHandler()
+
+    eh_parent._focused_child = eh_child
+    eh_child._focused_child = eh_grandchild
+
+    eh_child._parent = eh_parent
+    eh_grandchild._parent = eh_child
+
+    eh_child.register('event', mock_callback)
+    eh_parent.register('event', mock_alt_callback)
+
+    assert not Mock.mock_callback_was_called
+    assert not Mock.mock_alt_callback_was_called
+
+    eh_grandchild.trigger('event')
+
+    assert Mock.mock_callback_was_called
+    assert Mock.mock_alt_callback_was_called
+
+def test_EventHandler_triggers_events_in_proper_order():
+    """The EventHandler should trigger events in order.
+    First, it starts with the object that was triggered firing its capture
+    events. Then each child in turn fires its capture events. After firing its
+    capture events, the last child then fires its bubble events. Then each
+    parent in turn fires their own capture events starting with the immediate
+    parent and working until there are no more parents."""
+    Mock_reset()
+
+    eh_grandparent = TestImplementationOfEventHandler()
+    eh_parent = TestImplementationOfEventHandler()
+    eh_child = TestImplementationOfEventHandler()
+    eh_grandchild = TestImplementationOfEventHandler()
+
+    eh_grandparent._focused_child = eh_parent
+    eh_parent._focused_child = eh_child
+    eh_child._focused_child = eh_grandchild
+
+    eh_parent._parent = eh_grandparent
+    eh_child._parent = eh_parent
+    eh_grandchild._parent = eh_child
+
+    callback = TestCallback()
+
+    # Should not fire:
+    eh_grandparent.register('event', callback.c8, method=EVENT_CAPTURE)
+
+    # Should fire in this order:
+    eh_parent.register('event', callback.c1, method=EVENT_CAPTURE)
+    eh_child.register('event', callback.c2, method=EVENT_CAPTURE)
+    eh_grandchild.register('event', callback.c3, method=EVENT_CAPTURE)
+    eh_grandchild.register('event', callback.c4)  # Default (BUBBLE)
+    eh_child.register('event', callback.c5, method=EVENT_BUBBLE)
+    eh_parent.register('event', callback.c6, method=EVENT_BUBBLE)
+    eh_grandparent.register('event', callback.c7, method=EVENT_BUBBLE)
+
+    eh_parent.trigger('event')
+
+    assert callback.order == [1, 2, 3, 4, 5, 6, 7]
+
+def test_EventHandler_by_default_has_no_children():
+    """An EventHandler has no children by default. It's subclasses must
+    implement them."""
+    event_handler = EventHandler()
+    assert event_handler.has_child() == False
+    with pytest.raises(NotImplementedError):
+        event_handler.focused_child
+
+def test_EventHandler_by_default_has_no_parent():
+    """An EventHandler has no children by default. It's subclasses must
+    implement them."""
+    event_handler = EventHandler()
+    assert event_handler.has_parent() == False
+    with pytest.raises(NotImplementedError):
+        event_handler.parent
