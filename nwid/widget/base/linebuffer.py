829a04b62b8c247334f93d6cc96215f594e9e623
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-06-08 22:18:03 -0400

    Adds the skeleton for the LineBuffer object.

---
+++ nwid/widget/base/linebuffer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# name:             widget.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       06/08/2017
+#
+
+"""
+nwid.widget.base.linebuffer
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This module contains the LineBuffer object for buffering the terminal's lines
+of text.
+"""
+
+from __future__ import absolute_import
+
+
+class LineBuffer(object):
+    """TODO"""
