dfe3c9d5874360d7fdf3a0738901d24bfd4e9913
Harold Bradley III <hbradleyiii@bradleystudio.net>
2017-06-08 22:20:57 -0400

    Adds the skeleton for the Viewport object.

---
+++ tests/widget/base/test_viewport.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# name:             test_viewport.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       06/08/2017
+#
+
+"""
+Unittests for nwid.widget.base.viewport module.
+"""
