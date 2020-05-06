5b92ce5ad6329ddedfba2e9aac23691b92ec6a4e
Harold Bradley III <hbradleyiii@bradleystudio.net>
2020-05-05 21:47:36 -0400

    Updates setup.py.

--- setup.py
+++ setup.py
@@ -11,40 +11,41 @@ from codecs import open
 from setuptools import setup, find_packages
 from os import path
 
+from nwid import __version__ as version
+
 
 with open('README.rst', 'r', 'utf-8') as f:
     README = f.read()
-    
-HERE = path.abspath(path.dirname(__file__))
+
+url = 'https://github.com/hbradleyiii/nwid/archive/v{}.tar.gz'\
+        .format(version)
 
 setup(
     name = 'nwid',
-    version = '0.1',
+    version = version,
     description = 'A terminal widget framework for humans.',
     long_description = README,
+    long_description_content_type='text/x-rst',
     url = 'https://github.com/hbradleyiii/nwid',
-    download_url = 'https://github.com/hbradleyiii/nwid/archive/v0.1-planning.tar.gz',
+    download_url = url,
     author = 'Harold Bradley III',
-    author_email = 'harold@bradleystudio.net',
+    author_email = 'harold@prestix.studio',
     license = 'MIT License',
     keywords = ['server development', 'terminal programming', 'terminal', 'terminal widgets'],
-    classifiers = [  # See https://pypi.python.org/pypi?%3Aaction = list_classifiers
+    classifiers = [
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages = find_packages(),
     install_requires = [],
-    test_requires = ['pytest>=2.8.0', 'mock'],
+    test_requires = ['pytest>=3', 'mock'],
     package_data = { '' : ['LICENSE'], },
     entry_points = { },
 )
